6cdfe2db7df3a9c206a91cafc14c79e5cff833a9
felixfrohboese <felix.frohboese@gmail.com>
2020-07-12 17:00:40 +0200

    tests added

--- test_main.py
+++ test_main.py
@@ -19,22 +19,24 @@ def client():
     client = main.APP.test_client()
 
     yield client
-
+    assert False
 
 
 def test_health(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.json == 'Healthy'
+    assert False
 
 
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+    assert False
